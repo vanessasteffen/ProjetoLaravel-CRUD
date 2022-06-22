3ee0245c10a364af72fb7dc52c3b26958dfe28b6
Vanessa Steffen <vanessasteffen186@gmail.com>
2022-06-22 16:52:48 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,31 @@
 use App\Http\Controllers\ClienteApiController;
 use App\Http\Controllers\DocumentoApiController;
 use App\Http\Controllers\ProdutoApiController;
+use App\Http\Controllers\AuthenticateController;
 
 //Route:: get('/clientes', [ClienteApiController::class, 'index']);
 
-Route::get('clientes/{id}/documento', [ClienteApiController::class, 'documento']);
-Route::get('documento/{id}/cliente', [DocumentoApiController::class, 'cliente']);
-
-Route::get('produto/{id}/clientes', [ProdutoApiController::class, 'cliente']);
-Route::get('clientes/{id}/produto', [ClienteApiController::class, 'produto']);
-Route::apiResource('produto', ProdutoApiController::class);
+//rotas de login
+Route::post('login', [AuthenticateController::class,'authenticate']);
+Route::post('login-refresh', [AuthenticateController::class,'refreshToken']);
+Route::get('me', [AuthenticateController::class,'getAuthenticatedUser']);
 
 
-Route:: apiResource('clientes', ClienteApiController::class);
-Route:: apiResource('documento', DocumentoApiController::class);
+//Route::group(['middleware' => 'auth:api'],
+//function() {
+//Rotas de cliente
+    Route::get('clientes/{id}/documento', [ClienteApiController::class, 'documento']);
+    Route::get('clientes/{id}/produto', [ClienteApiController::class, 'produto']);
+    Route:: apiResource('clientes', ClienteApiController::class);
+
+//rotas de documento
+    Route::get('documento/{id}/cliente', [DocumentoApiController::class, 'cliente']);
+    Route:: apiResource('documento', DocumentoApiController::class);
+
+//rotas de produto
+    Route::get('produto/{id}/clientes', [ProdutoApiController::class, 'cliente']);
+    Route::apiResource('produto', ProdutoApiController::class);
+//});
+
 
 
